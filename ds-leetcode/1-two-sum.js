38b6f0ce1c8e3e3f5cd7c39c5b6f83630e44e887
zhutoutoutousan <mxyuiiu64545@126.com>
2022-06-05 18:19:58 +0800

    Aktualisierung

---
+++ ds-leetcode/1-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // O(n)
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (map[target - nums[i]] !== undefined) {
+            return [map[target - nums[i]], i];
+        }
+        map[nums[i]] = i;
+    }
+};
